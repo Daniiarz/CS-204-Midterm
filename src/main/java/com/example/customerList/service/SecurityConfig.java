7cae09c55043e9ea161ba3626fae6d432f0dfb2b
Daniiarz <daniyarflash.m01@gmail.com>
2020-04-03 15:32:18 +0600

    Fixed auth issue

--- src/main/java/com/example/customerList/service/SecurityConfig.java
+++ src/main/java/com/example/customerList/service/SecurityConfig.java
@@ -1,26 +1,16 @@
 package com.example.customerList.service;
 
-import com.example.customerList.repository.AdminRepository;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
 
-@Configuration
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    @Autowired
-    private AdminRepository adminRepository;
-
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-        http.
+        http.csrf().disable().
                 authorizeRequests()
                 .antMatchers("/").permitAll()
                 .antMatchers("/**").access("hasRole('ADMIN')")
@@ -29,13 +19,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-                .userDetailsService(new UserDetailsService() {
-                    @Override
-                    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-                        return adminRepository.findById(username).get();
-                    }
-                });
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception
+    {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("{noop}admin")
+                .roles("ADMIN");
     }
+
 }
